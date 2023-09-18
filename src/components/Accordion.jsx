6811bc93c91afc19e619c0407a138351b9228335
MAbdurahman <mdbdrrhm@gmail.com>
2023-09-18 06:04:13 -0500

    Modify title

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -5,7 +5,7 @@ export default function Accordion({data}) {
 
     return (
         <section className="accordion">
-            <h2 className="u-text-center">Frequent Ask Questions</h2>
+            <h2 className="u-text-center">Soft Skills For Computer Science</h2>
             {data.map((el, i) => (
                 <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
             ))}
--- src/data.js
+++ src/data.js
@@ -1,17 +1,25 @@
- export const data = [
+export const data = [
     {
-        title: "Where are these chairs assembled?",
+        title: "Attention To Detail",
         text:
-            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+            "In the computer science and IT field, attention to detail is vital. You must be able to look at long lines of code and identify anything amiss. Having an eye for small details can help you have a huge impact on projects. Listing on your résumé that you’re detail-oriented shows potential employers you understand how impactful small elements are to the larger project. Being a stickler for the details can be an immense strength."
     },
     {
-        title: "How long do I have to return my chair?",
+        title: "Creativity",
         text:
-            "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+            "Some people think computer science is all logical, dry work. But that’s simply not true. Creativity is key to solving problems and creating functional, modern web applications and websites. When you put creativity on your résumé, the hiring manager can see you’re someone who’ll think outside the box to solve problems and give unique solutions."
     },
     {
-        title: "Do you ship to countries outside the EU?",
+        title: "Communication",
         text:
-            "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+            "In the IT world, communication is key. This soft skill is vital to help you talk to clients, coworkers, and managers. Show you’re skilled when it comes to explaining your projects, objectives, and timelines. Listing communication on your résumé shows you recognize the importance of communicating clearly in your job. It’s wise to show employers that you have a willingness to engage respectfully in  workplace communication."
+    },
+    {
+        title: "Time Management",
+        text: "In most jobs in the computer science field, there are tight deadlines, competing priorities, and quick transitions between tasks. Effective time management will help you stay on top of the various tasks and projects you’ll face. By listing time management on your résumé, you show potential employers you’re dedicated to hitting deadlines and you’ll be productive and efficient in your work."
+    },
+    {
+        title: "Teamwork and Cooperation",
+        text: "Being a good team player is key in any organization, but it’s particularly important if you’re working in computer science. Many times, software engineers, data scientists, and IT professionals are expected to collaborate with a number of different groups within an organization to ensure timelines are met and expectations are fulfilled. "
     }
 ];
