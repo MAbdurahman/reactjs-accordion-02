16c70fab27f836b3fea564fb0374fb83aeb1eea8
MAbdurahman <mdbdrrhm@gmail.com>
2023-09-19 04:13:27 -0500

    Remove extraneous code from components/AccordionItem.jsx

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 export default function AccordionItem({currentOpen, onOpen, num, title, children}) {
     //**************** variables ****************//
